c38ab84f7e9f9e3f9fa9b1231f530b4c0ae045f8
Fofone9 <mr.paa2004@gmail.com>
2024-04-15 20:18:27 +0300

    сделал все сортировки

--- lab5/searches/digitalSort.py
+++ lab5/searches/digitalSort.py
@@ -11,4 +11,24 @@ class digitalSort:
             self.array.append(random.randint(0, 10))
 
     def sort(self):
-        pass
+        temp = [[] for k in range(10)]
+        n = len(str(max(self.array)))
+        for i in range (n):
+            for elem in self.array:
+                digit = (elem//10**i)%10
+                temp[digit].append((elem))
+            self.array = []
+            for row in temp:
+                self.array.extend(row)
+            temp = [[]for k in range(10)]
+
+    def show(self):
+        for i in self.array:
+            print(i, end=' ')
+        print()
+
+test = digitalSort()
+test.setArray(112)
+test.show()
+test.sort()
+test.show()
